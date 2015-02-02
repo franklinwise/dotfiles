9519184a54de0c76d40b252874922f869ddeb3c6
Franklin Wise <franklin@krave-n.com>
2015-02-02 13:20:38 -0800

    basic working version

--- home/bin/tabs_not_spaces.sh
+++ home/bin/tabs_not_spaces.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# forces every VIM plugin to use tabs and not spaces
+# obviously this would be problematic for yaml 
+
 oldstring=\bexpandtab\b
 newstring=noexpandtab
 grep -rl $oldstring ~/.vim/bundle | xargs sed -i s/$oldstring/$newstring/g

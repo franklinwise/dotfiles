0a9fe642bdd1ee481358f050cba934c0a4f4fb96
Franklin Wise <franklin@krave-n.com>
2015-02-02 20:12:41 -0800

    git ignore

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -18,7 +18,7 @@ set -xg EDITOR "mvim"
 set -xg GEM_HOME "$HOME/.chefdk/gem/ruby/2.1.0"
 set -xg GEM_PATH "$HOME/.chefdk/gem/ruby/2.1.0:/opt/chefdk/embedded/lib/ruby/gems/2.1.0"
 set -xg GEM_ROOT "/opt/chefdk/embedded/lib/ruby/gems/2.1.0"
-#set -xg XDG_CONFIG_HOME "$HOME/.config"
+set -xg XDG_CONFIG_HOME "$HOME/.config"
 
 
 #
